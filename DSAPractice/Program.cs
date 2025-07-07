f3bd439d6ffb448a34076838f867033f989572b3
Raju Chaudhary <raju.karkend@gmail.com>
2025-07-08 01:20:36 +0530

    frequecy count of each element

---
+++ DSAPractice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        var item = new int[] { 1, 4, 1, 2, 5, 3, 6 };
+        CountFrequencyUsingLinq(item);
+        CountFrequency(item);
+    }
+
+    private static void CountFrequencyUsingLinq(int[] item)
+    {
+        var freq = item.GroupBy(x => x).Select(g => new { Number = g.Key, Count = g.Count() }).OrderByDescending(x => x.Count);
+        foreach (var f in freq)
+            Console.WriteLine($"{f.Number} => {f.Count}");
+    }
+
+    private static void CountFrequency(int[] item)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var it in item)
+        {
+            if (dict.ContainsKey(it))
+            {
+                dict[it]++;
+            }
+            else
+            {
+                dict[it] = 1;
+            }
+        }
+
+        Console.WriteLine($"using dictinary approach");
+        // Optional: Print the frequencies
+        foreach (var kvp in dict)
+        {
+            Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+        }
+    }
+}
