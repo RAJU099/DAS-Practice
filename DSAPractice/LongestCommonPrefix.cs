2651f881eb6cf4c1f4fe410c448b84aac19023fd
Raju Chaudhary <raju.karkend@gmail.com>
2025-07-16 02:41:53 +0530

    longest common prefix in a list of string

---
+++ DSAPractice/LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DSAPractice
+{
+	public class LongestCommonPrefix
+	{
+		public static void CommonPrefix()
+		{
+            var strs = new string[] { "flower", "flow", "flight" };
+            Array.Sort(strs);
+            string first = strs[0];
+            string last = strs[^1];
+
+            int minLength = Math.Min(first.Length, last.Length);
+            int i = 0;
+            while (i < minLength && first[i] == last[i])
+            {
+                i++;
+            }
+            if(i == 0)
+            {
+                Console.WriteLine($"no longest common sufix exits");
+            }
+            else
+            {
+                Console.WriteLine($"longest common prefix is ---> {first.Substring(0,i)}");
+            }
+        }
+	}
+}
+
