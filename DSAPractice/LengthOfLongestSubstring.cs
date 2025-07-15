0f32da628ed7920e5af60c3ade3ec21a08012722
Raju Chaudhary <raju.karkend@gmail.com>
2025-07-16 02:43:00 +0530

    length of longest substrings with unique character

---
+++ DSAPractice/LengthOfLongestSubstring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DSAPractice
+{
+	public class LengthOfLongestSubstring
+	{
+		public static void LengthOfLongestSubstringallUnique()
+		{
+            int left = 0, right = 0, maxLength = 0;
+            string s = "abcabcbb";
+            HashSet<char> seen = new HashSet<char>();
+
+            while (right < s.Length)
+            {
+                if (!seen.Contains(s[right]))
+                {
+                    seen.Add(s[right]);
+                    maxLength = Math.Max(maxLength, right - left + 1);
+                    right++;
+                }
+                else
+                {
+                    seen.Remove(s[left]);
+                    left++;
+                }
+            }
+
+            Console.WriteLine($"maximum length of all non repeating string ---> {maxLength}");
+        }
+	}
+}
+
